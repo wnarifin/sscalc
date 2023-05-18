f4fd57c4308386051bd13fcb79dc71c2d906cf4c
wnarifin <wnarifin@gmail.com>
2023-05-18 14:29:55 +0800

    Signed-off-by: wnarifin <wnarifin@gmail.com>

--- R/hello.R
+++ R/hello.R
@@ -9,6 +9,6 @@ hello <- function(myname = ""){
     stop("Tell me your name!")
   }
   list(
-    message = paste("hello", myname, "! This is", R.Version()$version.string)
+    message = paste("Hello R user ", myname, "! This is", R.Version()$version.string)
   )
 }
